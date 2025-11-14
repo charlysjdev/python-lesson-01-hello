9414b9a2449ab053ec427b01b0f75ab7cb87e7e3
Carlos Santillan <61282212+Charles181@users.noreply.github.com>
2025-11-14 11:01:12 -0600

    initial commit
    Created initial file

---
+++ project.py
@@ -0,0 +1,13 @@
+def hello():
+    #
+    # THIS IS THE PART YOU NEED TO EDIT
+    #
+    return "Goodbye, world!" # This is wrong on purpose!
+
+# THIS SECTION BELOW SHOULD NOT BE MODIFIED
+def main():
+    # We call the hello() function and print its result.
+    # This is so you can rune your code and see what it does.
+    print(hello())
+if __name__ == "__main__":
+    main()
